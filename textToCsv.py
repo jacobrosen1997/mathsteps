8a1a819ca81257f71d29ee095440096c2ac6765a
jacobrosen1997 <jacob@matteappen.se>
2021-07-08 09:03:02 +0200

    update to testing.js script and textToXsv python

---
+++ textToCsv.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+
+with open('hints.txt',encoding='UTF-8') as f:
+    df_hints = f.read().split('\t')
+
+with open('actions.txt',encoding='UTF-8') as f:
+    df_actions = f.read().split('\t')
+
+hints = []
+for row in df_hints:
+    hints.append(row)
+
+actions = []
+for row in df_actions:
+    actions.append(row) 
+
+dlist = list(zip(actions,hints))
+columns = ["Actions","Hints"]
+df = pd.DataFrame(dlist,columns=columns)
+
+try:
+    df.to_csv("hints.csv",columns=columns)
+    print("Created file successfully.")
+except:
+    print("Didn't work, probably same file name.")
+
+
